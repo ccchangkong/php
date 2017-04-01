6cfe401441b926cdef240ad67c8b92427da29f17
ccchangkong <chlo0o@qq.com>
2017-04-01 14:05:29 +0800

    add

--- directory_submit.php
+++ directory_submit.php
@@ -17,7 +17,19 @@ if (!($ip = gethostbyname($host))) {
 }
 echo "host is at ip $ip <br>";
 
-$email = explode('@', $email)
+$email = explode('@', $email);
+$emailhost = $email[1];
+
+if (!dns_get_mx($emailhost, $mxhostsarr)) {
+	echo "email address is not at valid host";
+	exit;
+}
+echo "email is delivered via";
+foreach ($mxhostsarr as $$mx) {
+	echo "$mx";
+	echo "<br> all submitted details are ok<br>";
+	echo "thank you for submitting your site.<br>it will be visited by one of our staff members soon";
+}
 ?>
 </body>
 </html>
