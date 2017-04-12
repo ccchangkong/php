a7a592717b6e1e7f30aa310db9e02bccb0abe351
ccchangkong <chlo0o@qq.com>
2017-04-12 17:50:26 +0800

    add

---
+++ list_extension_funcs.php
@@ -0,0 +1,14 @@
+<?php
+echo "install:<br>";
+$extensions = get_loaded_extensions();
+foreach ($extensions as $each_ext) {
+	echo "$each_ext <br>";
+	echo "<ul>";
+	$ext_funcs = get_extension_funcs($each_ext);
+	foreach ($ext_funcs as $func) {
+		echo "<li>$func</li>";
+	}
+	echo "</ul>";
+}
+
+?>
